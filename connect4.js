ff54fefcc32461e36fd7f92cdc09e70095dbd871
marxmoyer <marxmoyer@gmail.com>
2023-03-24 14:40:35 -0600

    clean up and formatting

--- connect4.js
+++ connect4.js
@@ -15,30 +15,23 @@ const playerturn = document.querySelector("#playerNumber")
 let WIDTH = 7;
 let HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
+let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
 
-/** makeBoard: create in-JS board structure:
- *    board = array of rows, each row is array of cells  (board[y][x])
- */
-
-
-// TODO: set "board" to empty HEIGHT x WIDTH matrix array
+// makeBoard: create in-JS board structure:
 function makeBoard() {
-  for(let y = 0; y < HEIGHT; y++){
+  for(let y = 0; y < WIDTH; y++){
     board.push(Array.from({ length: HEIGHT }));
     
   }
 }
  
-
 /** makeHtmlBoard: make HTML table and row of column tops. */
-
 function makeHtmlBoard() {
   const table = document.querySelector("#board")
   
   // Add function the the top row and listens for a click of the top row
-  var top = document.createElement("tr");
+  const top = document.createElement("tr");
   top.setAttribute("id", "column-top");
   top.addEventListener("click", handleClick);
 
@@ -61,8 +54,7 @@ function makeHtmlBoard() {
   }
 }
 
-
-  // makes the div to track whos trun
+// makes the div to track whos trun
   const playerCount = document.createElement('div')
   playerCount.classList.add('turn','player1')
   playerturn.append(playerCount)
@@ -70,10 +62,7 @@ function makeHtmlBoard() {
 // updats plyers turn
 function upPlayerTurn(){
   playerCount.classList.toggle("player2")
- 
 }
-
-
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
@@ -82,14 +71,11 @@ function findSpotForCol(x) {
     if(!board ?.[x] ?.[y]){
       return y
     }
-
   }
   return null
 }
 
-
 /** placeInTable: update DOM to place piece into HTML table of board */
-
 function placeInTable(y, x) {
   const newPiece = document.createElement('div')
   newPiece.classList.add('piece')
@@ -97,19 +83,14 @@ function placeInTable(y, x) {
   
   const place = document.getElementById(`${y}-${x}`)
   place.append(newPiece)
-
-  
 }
 
 /** endGame: announce game end */
-
 function endGame(msg) {
   alert(msg)
-  
 }
 
 /** handleClick: handle click of column top to play piece */
-
 function handleClick(evt) {
   // get x from ID of clicked cell
   var x = +evt.target.id;
@@ -135,18 +116,13 @@ function handleClick(evt) {
     return endGame('Tie!');
   } 
   // switch players
-
   currPlayer = currPlayer === 1 ? 2 : 1
   upPlayerTurn()
-  
-
-  
-
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
-
 function checkForWin() {
+  
   function _win(cells) {
     // Check four cells to see if they're all color of current player
     //  - cells: list of four (y, x) cells
@@ -163,7 +139,6 @@ function checkForWin() {
   }
 
   // this bit of code looks at the 2d arr and checks all combnations of win condiction
-
   for (var y = 0; y < HEIGHT; y++) {
     for (var x = 0; x < WIDTH; x++) {
       var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
